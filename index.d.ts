82642f7e21c5f5436d3502724063b3e4326051a2
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2021-09-10 01:00:06 +0300

    add index.d.ts with declarations

---
+++ index.d.ts
@@ -0,0 +1,56 @@
+declare module '@rfcx/http-utils' {
+  class Conversion {
+    src: any
+    target: any
+    property: string
+    value: any
+    conversions: any[]
+    required: boolean
+    defaultValue: any
+    execute(): void
+    default(def: any): this
+    optional(): this
+    throwError(message: string): void
+    toFloat(): this
+    minimum(a: number): this
+    maximum(b: number): this
+    minLength(a: string): this
+    maxLength(b: string): this
+    nonEmpty(): this
+    toUuid(): this
+    toLatitude(): this
+    toLongitude(): this
+    toMoment(tz: string): this
+    toMomentUtc(): this
+    toDateString(): this
+    toTimeInterval(): this
+    toAggregateFunction(): this
+    toInt(): this
+    toNonNegativeInt(): this
+    toString(): this
+    toLowerCase(): this
+    toUpperCase(): this
+    trim(): this
+    objToString(): this
+    toBoolean(): this
+    toArray(): this
+    toIntArray(): this
+    toFloatArray(): this
+    nonEmptyArrayItem(): this
+    isObject(): this
+    isValidTimezone(): this
+    isEqualToAny(): this
+  }
+  class Converter {
+    constructor(validatedObject: any, transformedObject: any)
+    validatedObject: any
+    currentValue: any
+    currentProperty: string
+    transformedObject: any
+    conversions: any[]
+    convert(property: string): Conversion
+    validate(): any
+  }
+  function httpError (req: Object, res: Object, code: number, context: string, mes: string): void
+  function httpErrorHandler (req: Object, res: Object, fallbackMessage: string): typeof httpError
+}
